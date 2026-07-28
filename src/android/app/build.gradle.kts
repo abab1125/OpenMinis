a524dc044958e78c8c777d9f394b91163a5821ca
ma <ma@madeMac-mini.local>
2026-07-28 09:10:25 +0800

    chore: bump version to 0.25

--- src/android/app/build.gradle.kts
+++ src/android/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.openminis.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 24
-        versionName = "0.24"
+        versionCode = 25
+        versionName = "0.25"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
